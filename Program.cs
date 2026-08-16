7800e46ca755fe8f4d558d6e2fdb34fe6026b10f
mirna <mirna@DESKTOP-2CFC66T>
2026-08-16 14:08:47 -0800

    ..

--- Program.cs
+++ Program.cs
@@ -23,7 +23,6 @@ using Microsoft.AspNetCore.Http;
 
 var builder = WebApplication.CreateBuilder(args);
 var configuration = builder.Configuration;
-
 // Connection String
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
